5ef3e77610cc29e098d91abdb5b39b54e5b3ed51
rezabarzgar <reza.barzgar99@gmail.com>
2023-02-24 11:40:51 -0500

    adding test func

--- src/models/ann.py
+++ src/models/ann.py
@@ -9,7 +9,6 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 
-
 logger = logging.getLogger()
 
 class SimpleANN(torch.nn.Module, Baseline):
@@ -42,6 +41,7 @@ class SimpleANN(torch.nn.Module, Baseline):
             x = self.activation(x)
 
         x = self.layers[-1](x)
+        x = torch.softmax(x, dim=1)
         return x
 
     def learn(self, epoch_num: int, batch_size: int):
@@ -55,21 +55,46 @@ class SimpleANN(torch.nn.Module, Baseline):
         """
         recall = torchmetrics.Recall(task='multiclass', num_classes=2)
         logger.info("training phase started")
-        for i in range(1, epoch_num+1):
+        for i in range(1, epoch_num + 1):
             loss = 0
             train_dataloader = DataLoader(self.train_dataset, batch_size, shuffle=True)
-            for X, y in train_dataloader:
+            for batch_index, (X, y) in enumerate(train_dataloader):
                 y_hat = self.forward(X)
                 self.optimizer.zero_grad()
                 loss = self.loss_function(y_hat, y)
                 loss.backward()
                 self.optimizer.step()
-                logger.info(f'recall on batch: {recall(y_hat.argmax(1), y)}')
+                # logger.info(f'recall on batch: {recall(y_hat.argmax(1), y)}')
+                logger.info(f"epoch: {i} | batch: {batch_index} | loss: {loss}")
 
             logger.info(f'epoch {i}:\n Loss: {loss}')
 
-    def test(self):
-        pass
+    def test(self, test_dataset):
+        accuracy = torchmetrics.Accuracy('binary',)
+        precision = torchmetrics.Precision('binary',)
+        recall = torchmetrics.Recall('binary',)
+        all_preds = []
+        all_targets = []
+        test_dataloader = DataLoader(test_dataset, batch_size=64)
+        size = len(test_dataset)
+        num_batches = len(test_dataloader)
+        test_loss, correct = 0, 0
+        with torch.no_grad():
+            for X, y in test_dataloader:
+                pred = self.forward(X)
+                all_preds.extend(pred.argmax(1))
+                all_targets.extend(y)
+                test_loss += self.loss_function(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        test_loss /= num_batches
+        correct /= size
+        all_preds = torch.tensor(all_preds)
+        all_targets = torch.tensor(all_targets)
+        logger.info(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        logger.info(f'torchmetrics Accuracy: {(100 * accuracy(all_preds, all_targets)):>0.1f}')
+        logger.info(f'torchmetrics precision: {(100 * precision(all_preds, all_targets)):>0.1f}')
+        logger.info(f'torchmetrics Recall: {(100 * recall(all_preds, all_targets)):>0.1f}')
+
 
     def get_session_path(self, file_name: str = "") -> str:
         return self.preprocessed_path + file_name
