571db71214c2b2e5d1e67be2368e9bf3e6eb5405
Hamed Waezi <hamedwaezi01@gmail.com>
2023-03-01 17:37:31 -0500

    init char repititino remover

---
+++ src/preprocessing/repetitions.py
@@ -0,0 +1,16 @@
+import re
+from preprocessing.base import BasePreprocessing
+
+class RepetitionRemoving(BasePreprocessing):
+
+    def opt(self, input):
+        for record in input:
+            result = []
+            for token in record:
+                for instance, letter in re.findall(r'((\w)\2{2,})', token):
+                    token = token.replace(instance, letter)
+                result.append(token)
+            yield result
+    
+    def name(self) -> str:
+        return "repetition remover"
