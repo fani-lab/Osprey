56489251beee41bda93e5d13f4412407b2041241
rezabarzgar <reza.barzgar99@gmail.com>
2023-03-07 11:45:22 -0500

    adding k_fold cross validation to ann model

--- src/models/ann.py
+++ src/models/ann.py
@@ -10,6 +10,8 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 
+from sklearn.model_selection import KFold
+
 logger = logging.getLogger()
 
 
@@ -49,31 +51,84 @@ class SimpleANN(torch.nn.Module, Baseline):
         x = torch.softmax(x, dim=1)
         return x
 
-    def learn(self, epoch_num: int, batch_size: int):
-        """
-        This function is the training phase of the model
-
-        Args:
-             epoch_num: number of epochs
-             batch_size: size of training batches
-
-        """
-        recall = torchmetrics.Recall(task='multiclass', num_classes=2)
+    def learn(self, epoch_num: int, batch_size: int, k_fold: int):
+        accuracy = torchmetrics.Accuracy('binary', )
+        precision = torchmetrics.Precision('binary', )
+        recall = torchmetrics.Recall('binary', )
         logger.info("training phase started")
-        for i in range(1, epoch_num + 1):
-            loss = 0
-            train_dataloader = DataLoader(self.train_dataset, batch_size, shuffle=True)
-            for batch_index, (X, y) in enumerate(train_dataloader):
-                y_hat = self.forward(X)
-                self.optimizer.zero_grad()
-                loss = self.loss_function(y_hat, y)
-                loss.backward()
-                self.optimizer.step()
-                logger.info(f"epoch: {i} | batch: {batch_index} | loss: {loss}")
+        kfold = KFold(n_splits=k_fold)
+        for fold, (train_ids, validation_ids) in enumerate(kfold.split(self.train_dataset)):
+            logger.info(f'FOLD {fold}')
+            logger.info('--------------------------------')
+            train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+            validation_subsampler = torch.utils.data.SubsetRandomSampler(validation_ids)
+            train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size,
+                                                       sampler=train_subsampler)
+            validation_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size,
+                                                            sampler=validation_subsampler)
+            # Train phase
+            for i in range(1, epoch_num + 1):
+                loss = 0
+                for batch_index, (X, y) in enumerate(train_loader):
+                    y_hat = self.forward(X)
+                    self.optimizer.zero_grad()
+                    loss = self.loss_function(y_hat, y)
+                    loss.backward()
+                    self.optimizer.step()
+                    logger.info(f"epoch: {i} | batch: {batch_index} | loss: {loss}")
 
-            logger.info(f'epoch {i}:\n Loss: {loss}')
+                logger.info(f'epoch {i}:\n Loss: {loss}\n')
+
+                # Validation phase
+                all_preds = []
+                all_targets = []
+                size = len(validation_loader)
+                num_batches = len(validation_loader)
+                test_loss, correct = 0, 0
+                with torch.no_grad():
+                    for batch_index, (X, y) in enumerate(validation_loader):
+                        pred = self.forward(X)
+                        all_preds.extend(pred.argmax(1))
+                        all_targets.extend(y)
+                        test_loss += self.loss_function(pred, y).item()
+                        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+                test_loss /= num_batches
+                correct /= size
+                all_preds = torch.tensor(all_preds)
+                all_targets = torch.tensor(all_targets)
+                logger.info(f"Validation Error: Avg loss: {test_loss:>8f}")
+                logger.info(f'torchmetrics Accuracy: {(100 * accuracy(all_preds, all_targets)):>0.1f}')
+                logger.info(f'torchmetrics precision: {(100 * precision(all_preds, all_targets)):>0.1f}')
+                logger.info(f'torchmetrics Recall: {(100 * recall(all_preds, all_targets)):>0.1f}\n')
+
         current_time = time.strftime("%m-%d-%Y-%H-%M", time.localtime())
         self.save(path=f"output/ann/ann-{current_time}.pth")
+
+    # def learn(self, epoch_num: int, batch_size: int):
+    #     """
+    #     This function is the training phase of the model
+    #
+    #     Args:
+    #          epoch_num: number of epochs
+    #          batch_size: size of training batches
+    #
+    #     """
+    #     recall = torchmetrics.Recall(task='multiclass', num_classes=2)
+    #     logger.info("training phase started")
+    #     for i in range(1, epoch_num + 1):
+    #         loss = 0
+    #         train_dataloader = DataLoader(self.train_dataset, batch_size, shuffle=True)
+    #         for batch_index, (X, y) in enumerate(train_dataloader):
+    #             y_hat = self.forward(X)
+    #             self.optimizer.zero_grad()
+    #             loss = self.loss_function(y_hat, y)
+    #             loss.backward()
+    #             self.optimizer.step()
+    #             logger.info(f"epoch: {i} | batch: {batch_index} | loss: {loss}")
+    #
+    #         logger.info(f'epoch {i}:\n Loss: {loss}')
+    #     current_time = time.strftime("%m-%d-%Y-%H-%M", time.localtime())
+    #     self.save(path=f"output/ann/ann-{current_time}.pth")
 
     def test(self, test_dataset):
         accuracy = torchmetrics.Accuracy('binary', )
@@ -117,4 +172,3 @@ class SimpleANN(torch.nn.Module, Baseline):
             logger.info("parameters loaded successfully")
         except Exception as e:
             logger.debug(e)
-
