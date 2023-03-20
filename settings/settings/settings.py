79a671fc85a26c4f8c44983683e4fabbdd94d8fb
rezabarzgar <reza.barzgar99@gmail.com>
2023-03-20 19:54:08 -0400

    adding the ability to run on GPU

--- settings/settings/settings.py
+++ settings/settings/settings.py
@@ -1,24 +1,23 @@
 TRAIN = 1
-TEST  = 2
-EVAL  = 4
-
+TEST = 2
+EVAL = 4
 
 preconfiged_sessions = {
-    "sample":{
+    "sample": {
         "commands": [
-            ("train", {"epoch_num": 100, "batch_size": 64, "k_fold": 10}, "bow-v0"),
-            ("test", {}, "test_bow")],
-        
+            ("train", {"epoch_num": 100, "batch_size": 64, "k_fold": 10}, "bow-onehot"),
+            ("test", {}, "bow-onehot")],
+
         "model_configs": {
             # Custom configs of a model as dict
         },
     },
     "ann": {
         "commands": [
-            ("train", {"epoch_num": 110, "batch_size": 500, "k_fold": 10}, "bow-v0"),
-            ("test", dict(), "bow-v0"),
+            ("train", {"epoch_num": 10, "batch_size": 500, "k_fold": 10}, "bow-onehot"),
+            ("test", dict(), "bow-onehot"),
             # ("eval", {"path": 'output/ann/'}, ""),
-            ],
+        ],
         "model_configs": {
             "dimension_list": list([32]),
             "activation": ("relu", dict()),
@@ -33,35 +32,35 @@ preconfiged_sessions = {
 }
 
 preconfiged_datasets = {
-    "bow-v0": (
-        "bow",  # short name of the dataset
-        {       # train configs
-            "data_path": "data/toy.train/toy-train.csv",
-            "output_path": "data/preprocessed/ann/",
-            "load_from_pkl": True,
-            "preprocessings": ["sw", "pr", "rr"],
-            "persist_data": True,
-        },
-        {      # test configs
-            "data_path": "data/toy.test/toy-test.csv",
-            "output_path": "data/preprocessed/ann/test-",
-            "load_from_pkl": True,
-            "preprocessings": ["sw", "pr", "rr"],
-            "persist_data": True,
-        }
-    ),
+    # "bow-v0": (
+    #     "bow",  # short name of the dataset
+    #     {  # train configs
+    #         "data_path": "data/train/train.csv",
+    #         "output_path": "data/preprocessed/ann/",
+    #         "load_from_pkl": True,
+    #         "preprocessings": ["sw", "pr", "rr"],
+    #         "persist_data": True,
+    #     },
+    #     {  # test configs
+    #         "data_path": "data/test/test.csv",
+    #         "output_path": "data/preprocessed/ann/test-",
+    #         "load_from_pkl": True,
+    #         "preprocessings": ["sw", "pr", "rr"],
+    #         "persist_data": True,
+    #     }
+    # ),
 
     "bow-onehot": (
         "bow",  # short name of the dataset
-        {       # train configs
-            "data_path": "data/toy.train/toy-train.csv",
+        {  # train configs
+            "data_path": "data/train/train.csv",
             "output_path": "data/preprocessed/ann/",
             "load_from_pkl": True,
             "preprocessings": ["sw", "pr", "rr"],
             "persist_data": True,
         },
-        {      # test configs
-            "data_path": "data/toy.test/toy-test.csv",
+        {  # test configs
+            "data_path": "data/test/test.csv",
             "output_path": "data/preprocessed/ann/test-",
             "load_from_pkl": True,
             "preprocessings": ["sw", "pr", "rr"],
@@ -69,33 +68,33 @@ preconfiged_datasets = {
         }
     ),
 
-    "bert-base-cased": (
-        "tranformer/bert-base-cased",  # short name of the dataset
-        {       # train configs
-            "data_path": "data/toy.train/toy-train.csv",
-            "output_path": "data/preprocessed/ann/",
-            "load_from_pkl": True,
-            "preprocessings": ["sw", "pr", "rr"],
-            "persist_data": True,
-        },
-        {      # test configs
-            "data_path": "data/toy.test/toy-test.csv",
-            "output_path": "data/preprocessed/ann/test-",
-            "load_from_pkl": True,
-            "preprocessings": ["sw", "pr", "rr"],
-            "persist_data": True,
-        }
-    ),
+    # "bert-base-cased": (
+    #     "tranformer/bert-base-cased",  # short name of the dataset
+    #     {  # train configs
+    #         "data_path": "data/toy.train/toy-train.csv",
+    #         "output_path": "data/preprocessed/ann/",
+    #         "load_from_pkl": True,
+    #         "preprocessings": ["sw", "pr", "rr"],
+    #         "persist_data": True,
+    #     },
+    #     {  # test configs
+    #         "data_path": "data/toy.test/toy-test.csv",
+    #         "output_path": "data/preprocessed/ann/test-",
+    #         "load_from_pkl": True,
+    #         "preprocessings": ["sw", "pr", "rr"],
+    #         "persist_data": True,
+    #     }
+    # ),
 }
 
 sessions = {
     "ann-onehot": {
         "model": "ann",
         "commands": [
-            ("train", {"epoch_num": 110, "batch_size": 500, "k_fold": 10}, "bow-onehot"),
+            ("train", {"epoch_num": 10, "batch_size": 500, "k_fold": 10}, "bow-onehot"),
             ("test", dict(), "bow-onehot"),
             ("eval", {"path": 'output/ann/'}, ""),
-            ],
+        ],
         "model_configs": {
             "dimension_list": list([32]),
             "activation": ("relu", dict()),
