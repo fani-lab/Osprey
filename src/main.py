2ed453243cb57ab27d3dc0969385d387866e56fd
Hamed Waezi <hamedwaezi01@gmail.com>
2023-03-28 17:15:09 -0400

    remove unused codes

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ from src.preprocessing.stopwords import NLTKStopWordRemoving
 from src.preprocessing.punctuations import PunctuationRemoving
 from src.preprocessing.repetitions import RepetitionRemoving
 
-from src.models.rnn import RnnModule
 from src.utils.dataset import BagOfWordsDataset, TimeBasedBagOfWordsDataset, TransformersEmbeddingDataset
 
 from settings import settings, mappings
@@ -34,54 +33,6 @@ logger.addHandler(debug_file_handler)
 logger.addHandler(info_terminal_handler)
 logger.setLevel(logging.DEBUG)
 
-def main():
-    test_path, train_path = "data/toy.test/toy-test.csv", "data/toy.train/toy-train.csv"
-    # test_path, train_path = "data/test/test.csv", "data/train/train.csv"
-    logger.info("reading test csv file")
-    test_df = pd.read_csv(test_path)
-    logger.info("reading train csv file")
-    train_df = pd.read_csv(train_path)
-    logger.debug("reading test and train csv files is done")
-
-    datasets_path = "data/preprocessed/ann/"
-
-    train_dataset = TimeBasedBagOfWordsDataset(train_path, datasets_path, False,
-                                               preprocessings=[NLTKStopWordRemoving(), PunctuationRemoving(), RepetitionRemoving()])
-    train_dataset.prepare()
-    test_dataset = TimeBasedBagOfWordsDataset(test_path, datasets_path + "test-", False,
-                                              preprocessings=[NLTKStopWordRemoving(), PunctuationRemoving(), RepetitionRemoving()],
-                                              parent_dataset=train_dataset)
-    test_dataset.prepare()
-    ## data_size, hidden_size, output_size, activation, loss_func, lr, train: pd.DataFrame, test: pd.DataFrame, preprocessings = list[BasePreprocessing], copy = True, load_from_pkl = True, preprocessed_path = "data/preprocessed/basic/"
-    # kwargs = {
-    #     "input_size": train_dataset.shape[1],
-    #     "hidden_dim": 64,
-    #     "num_layers": 1,
-    #     "learning_batch_size": 64,
-    #     "activation": torch.nn.ReLU(),
-    #     "loss_func": torch.nn.CrossEntropyLoss(),
-    #     "lr": 0.001,
-    #     "train_dataset": train_dataset,
-    #     "module_session_path": "/output/rnn",
-    #     "number_of_classes": 2,
-    # }
-
-
-    kwargs = {
-        # "dimension_list": list([950, 250, 150, 50, 2]),
-        "dimension_list": list([128]),
-        "activation": torch.nn.ReLU(),
-        "loss_func": torch.nn.CrossEntropyLoss(),
-        "lr": 0.1,
-        "train_dataset": train_dataset,
-        "module_session_path": f"output/{START_TIME}/",
-        "number_of_classes": 2,
-    }
-    # model = ANNModule(**kwargs)
-
-    # model.learn(epoch_num=100, batch_size=64, k_fold=10)
-    # model.test(test_dataset)
-    logger.info('Done!')
 
 def create_model_configs(session_name: str, session: dict, device: str):
     activation, activation_kwargs = mappings.ACTIVATIONS[session["model_configs"]["activation"][0]], session["model_configs"]["activation"][1]
