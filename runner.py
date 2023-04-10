cdd9dbeef9e97039610a6a1108830c7530b75911
Hamed Waezi <hamedwaezi01@gmail.com>
2023-04-10 13:45:52 -0400

    register new classes

--- runner.py
+++ runner.py
@@ -2,10 +2,11 @@
 from src.main import run
 from src.preprocessing import NLTKStopWordRemoving, PunctuationRemoving, RepetitionRemoving
 from src.utils.dataset import (BagOfWordsDataset, TimeBasedBagOfWordsDataset, TransformersEmbeddingDataset,
-                               CaseSensitiveBertEmbeddingDataset, GloveEmbeddingDataset, ConversationBagOfWords)
+                               CaseSensitiveBertEmbeddingDataset, GloveEmbeddingDataset, ConversationBagOfWords,
+                               CNNConversationBagOfWords)
 from src.utils.loss_functions import WeightedBinaryCrossEntropy
 from src.utils.commons import message_csv2conversation_csv, force_open
-from src.models import ANNModule, RnnModule
+from src.models import ANNModule, RnnModule, EbrahimiCNN
 from settings.mappings import register_mappings, register_mappings_torch
 
 
@@ -33,10 +34,12 @@ if __name__ == "__main__":
     register_mappings(CaseSensitiveBertEmbeddingDataset)
     register_mappings(GloveEmbeddingDataset)
     register_mappings(ConversationBagOfWords)
+    register_mappings(CNNConversationBagOfWords)
 
     register_mappings(WeightedBinaryCrossEntropy)
 
     register_mappings(ANNModule)
     register_mappings(RnnModule)
+    register_mappings(EbrahimiCNN)
 
     run()
