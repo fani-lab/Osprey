39d029cde6d01876c0c9cd484425a27041a94623
Hamed Waezi <hamedwaezi01@gmail.com>
2023-04-10 13:42:48 -0400

    a more general one-hot encoder

--- src/utils/one_hot_encoder.py
+++ src/utils/one_hot_encoder.py
@@ -5,32 +5,42 @@ from heapq import nlargest
 
 class OneHotEncoder:
 
-    def __init__(self, buffer_cap=20, device="cpu", vector_size=-1) -> None:
+    def __init__(self, buffer_cap=20, device="cpu", vector_size=-1, default_vectors_dimension=[1, 1]) -> None:
         
         self.__buffer_cap = buffer_cap
         self.vector_size = vector_size
         self.records = dict()
         self.device = device
         self.transform_started = False
-        self.__vectors_dimension = (1,1)
+        self.__vectors_dimension = default_vectors_dimension
         self.vectors = dict()
         self.__default_vector = None
         self.__zero_vector = None
     
+    def __create_sparse_vector(self, index):
+        if index is None:
+            ones = [[] for i in range(len(self.__vectors_dimension))]
+        else:
+            ones = [(0,) if i!=1 else (index,) for i in range(len(self.__vectors_dimension))]
+        return sparse_coo_tensor(ones, (1.0,)*(index != None), size=self.__vectors_dimension, dtype=float32)
+
     def __generate_sparse_vectors(self):
         for i, (k, _) in enumerate(self.records.items()):
-            self.vectors[k] = sparse_coo_tensor(((0,), (i,)),(1.0), size=self.__vectors_dimension, dtype=float32)
+            self.vectors[k] = self.__create_sparse_vector(i)
+            # self.vectors[k] = sparse_coo_tensor(((0,), (i,)),(1.0), size=self.__vectors_dimension, dtype=float32)
         
         del self.records
     
     def __get_default_vector(self):
         if self.__default_vector is None:
-            self.__default_vector = sparse_coo_tensor(((0,), (self.__vectors_dimension[1]-1,)),(1.0), size=self.__vectors_dimension, dtype=float32)
+            self.__default_vector = self.__create_sparse_vector(self.__vectors_dimension[1]-1)
+            # self.__default_vector = sparse_coo_tensor(((0,), (self.__vectors_dimension[1]-1,)),(1.0), size=self.__vectors_dimension, dtype=float32)
         return self.__default_vector
 
     def __get_zero_vector(self):
         if self.__zero_vector is None:
-            self.__zero_vector = sparse_coo_tensor((([], [])),[], size=self.__vectors_dimension, dtype=float32)
+            self.__zero_vector = self.__create_sparse_vector(None)
+            # self.__zero_vector = sparse_coo_tensor(([], []),[], size=self.__vectors_dimension, dtype=float32)
         return self.__zero_vector
 
     def transform(self, record):
@@ -69,4 +79,4 @@ class OneHotEncoder:
             mostfrequent = nlargest(self.vector_size, all_tokens_count, key=lambda x:x[1])
             self.records = {entry[0]: entry[1] for entry in mostfrequent}
 
-        self.__vectors_dimension = (1, len(self.records) + 1)
+        self.__vectors_dimension[1] = len(self.records) + 1
