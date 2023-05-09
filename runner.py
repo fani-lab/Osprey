29c9acb99bd20f535a23646ece0fffa62d933d00
Hamed Waezi <hamedwaezi01@gmail.com>
2023-05-09 12:35:08 -0400

    register new modules

--- runner.py
+++ runner.py
@@ -1,14 +1,14 @@
 # This file is for sake of compatibility between different development environment
 from src.main import run
-from src.preprocessing import NLTKStopWordRemoving, PunctuationRemoving, RepetitionRemoving
+from src.preprocessing import NLTKStopWordRemoving, PunctuationRemoving, RepetitionRemoving, AuthorIDReplacer
 from src.utils.dataset import (BagOfWordsDataset, TimeBasedBagOfWordsDataset, TransformersEmbeddingDataset,
                                CaseSensitiveBertEmbeddingDataset, GloveEmbeddingDataset, ConversationBagOfWords,
-                               CNNConversationBagOfWords, ConversationBagOfWordsCleaned)
+                               CNNConversationBagOfWords, ConversationBagOfWordsCleaned, SequentialConversationDataset)
 from src.utils.loss_functions import WeightedBinaryCrossEntropy
-from src.models import ANNModule, RnnModule, EbrahimiCNN
+from src.models import ANNModule, EbrahimiCNN, BaseRnnModule
 from settings.mappings import register_mappings, register_mappings_torch
 from src.scripts import create_conversations, balance_datasets_for_version_two, create_conversation_toy_set, generate_stats
-
+from src.utils.dataset import SequentialConversationDataset
 
 if __name__ == "__main__":
     # create_conversations()
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     register_mappings(NLTKStopWordRemoving)
     register_mappings(PunctuationRemoving)
     register_mappings(RepetitionRemoving)
+    register_mappings(AuthorIDReplacer)
 
     register_mappings(BagOfWordsDataset)
     register_mappings(TimeBasedBagOfWordsDataset)
@@ -26,12 +27,13 @@ if __name__ == "__main__":
     register_mappings(ConversationBagOfWords)
     register_mappings(CNNConversationBagOfWords)
     register_mappings(ConversationBagOfWordsCleaned)
+    register_mappings(SequentialConversationDataset)
 
     register_mappings(WeightedBinaryCrossEntropy)
 
     register_mappings(ANNModule)
-    register_mappings(RnnModule)
     register_mappings(EbrahimiCNN)
+    register_mappings(BaseRnnModule)
 
     run()
     # balance_datasets_for_version_two()
