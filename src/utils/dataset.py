67f6b16f6fb9d93908b058642885031c510ed3cc
Hamed Waezi <hamedwaezi01@gmail.com>
2023-06-06 14:05:32 -0400

    feature vector with context length bug

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset
 from sklearn.model_selection import KFold, StratifiedKFold
 
 from src.preprocessing.base import BasePreprocessing
-from src.utils.one_hot_encoder import OneHotEncoder, SequentialOneHotEncoder
+from src.utils.one_hot_encoder import OneHotEncoder, SequentialOneHotEncoder, SequentialOneHotEncoderWithContext
 from src.utils.transformers_encoders import TransformersEmbeddingEncoder, GloveEmbeddingEncoder
 from src.utils.commons import nltk_tokenize, force_open, RegisterableObject
 
@@ -119,6 +119,7 @@ class BaseDataset(Dataset, RegisterableObject):
         return self.short_name() +"/p" + ".".join([pp.short_name() for pp in self.preprocessings]) + "-v" + str(self.vector_size)
     
     def filter_records(self, df):
+        logger.info(f"no filter is applied to dataset: {self.short_name()}")
         return df
 
     def get_session_path(self, filename) -> str:
@@ -478,6 +479,10 @@ class SequentialConversationDataset(BaseDataset):
         super().__init__(data_path, output_path, load_from_pkl, apply_record_filter, preprocessings, persist_data, parent_dataset, device, *args, **kwargs)
         self.__sequence__ = None
 
+    def filter_records(self, df):
+        logger.info("applying record filtering by 'nauthor == 2'")
+        return df[(df["nauthor"] == 2)]
+
     @property
     def sequence(self):
         if self.__sequence__ is None:
@@ -518,9 +523,6 @@ class SequentialConversationDataset(BaseDataset):
     def tokenize(self, input) -> list[list[str]]:
         return nltk_tokenize(input)
 
-    # def normalize_vector(self, vectors):
-    #     return [vector/torch.sparse.sum(vector) for vector in vectors]
-
     def init_encoder(self, tokens_records):
         encoder = SequentialOneHotEncoder(vector_size=self.vector_size)
         logger.info("started generating bag of words vector encoder")
@@ -557,7 +559,7 @@ class SequentialConversationDataset(BaseDataset):
         return (len(self.data), -1, self.data[0].shape[-1])
 
 class TemporalSequentialConversationDataset(SequentialConversationDataset):
-
+    CONTEXT_LENGTH = 1
     @classmethod
     def short_name(cls) -> str:
         return "temporal-sequential"
@@ -570,6 +572,14 @@ class TemporalSequentialConversationDataset(SequentialConversationDataset):
                         yield pattern(token)
 
         return func
+    
+    def init_encoder(self, tokens_records):
+        encoder = SequentialOneHotEncoderWithContext(context_length=self.CONTEXT_LENGTH, vector_size=self.vector_size, )
+        logger.info("started generating bag of words vector encoder")
+        pattern = lambda x: x
+        logger.debug("fitting data into one hot encoder")
+        encoder.fit(self.get_data_generator(data=tokens_records, pattern=pattern))
+        return encoder
 
     def preprocess(self):
         try:
@@ -581,7 +591,11 @@ class TemporalSequentialConversationDataset(SequentialConversationDataset):
         except FileNotFoundError:
             logger.info("generating tokens from scratch")
             self.__new_tokens__ = True
-            messages = [(g["time"].tolist(), self.tokenize(g["text"])) for k, g in self.sequence]
+            messages = [None] * len(self.sequence)
+            for i, (k, g) in enumerate(self.sequence):
+                temp = np.floor(g["time"].tolist())
+                messages[i] = (temp + (g["time"].tolist()- temp) / 0.6, self.tokenize(g["text"]))
+            # messages = [((g["time"].tolist(),), self.tokenize(g["text"])) for k, g in self.sequence]
             logger.info("applying preprocessing modules")
             for preprocessor in self.preprocessings:
                 logger.info(f"applying {preprocessor.name()}")
@@ -592,12 +606,11 @@ class TemporalSequentialConversationDataset(SequentialConversationDataset):
         logger.debug("started transforming message records into sparse vectors")
         vectors = []
         
-        for i, (time, record) in enumerate(tokens_records):
+        for i, record in enumerate(tokens_records):
             sequence = encoder.transform(record=record)
             onehots = torch.stack([torch.sparse.sum(torch.cat(t), dim=0) for t in sequence])
-            temp = np.floor(time)
-            context = torch.tensor(temp + (time - temp) / 0.6, dtype=torch.float32).reshape(-1, 1).to_sparse_coo()
-            
-            vectors.append(torch.hstack((context, onehots)))
+
+            vectors.append(onehots)
+
         logger.debug("transforming of records into vectors is finished")
         return vectors
--- src/utils/one_hot_encoder.py
+++ src/utils/one_hot_encoder.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from heapq import nlargest
 
+NUMBER_OF_PREDEFINED_VECTORS = 1 # Should be handled dynamically later. needs to synced with the context vector
 
 class OneHotEncoder:
 
@@ -13,37 +14,40 @@ class OneHotEncoder:
         self.records = dict()
         self.device = device
         self.transform_started = False
-        self.__vectors_dimension = [1] * vectors_dimensions
+        self.vectors_dimension = [1] * vectors_dimensions
         self.vectors = dict()
         self.__default_vector = None
         self.__zero_vector = None
     
-    def __create_sparse_vector(self, index):
+    def create_sparse_vector(self, index):
         if index is None:
-            ones = [[] for i in range(len(self.__vectors_dimension))]
+            ones = [[] for i in range(len(self.vectors_dimension))]
         else:
-            ones = [(0,) if i!=1 else (index,) for i in range(len(self.__vectors_dimension))]
-        return sparse_coo_tensor(ones, (1.0,)*(index != None), size=self.__vectors_dimension, dtype=float32)
+            ones = [(0,) if i!=1 else (index,) for i in range(len(self.vectors_dimension))]
+        return sparse_coo_tensor(ones, (1.0,)*(index != None), size=self.vectors_dimension, dtype=float32)
 
-    def __generate_sparse_vectors(self):
+    def generate_sparse_vectors(self):
         for i, (k, _) in enumerate(self.records.items()):
-            self.vectors[k] = self.__create_sparse_vector(i)
+            self.vectors[k] = self.create_sparse_vector(i)
         
         del self.records
     
     def __get_default_vector(self):
         if self.__default_vector is None:
-            self.__default_vector = self.__create_sparse_vector(self.__vectors_dimension[1]-1)
+            self.__default_vector = self.create_sparse_vector(self.vectors_dimension[1]-1)
         return self.__default_vector
 
     def __get_zero_vector(self):
         if self.__zero_vector is None:
-            self.__zero_vector = self.__create_sparse_vector(None)
+            self.__zero_vector = self.create_sparse_vector(None)
         return self.__zero_vector
 
+    def get_number_of_predefined_vectors(self):
+        return 1 # the `1` is for the default vector; look at `__get_default_vector`
+    
     def transform(self, record):
         if not self.transform_started:
-            self.__generate_sparse_vectors()
+            self.generate_sparse_vectors()
         self.transform_started = True
         default_vector = self.__get_default_vector()
         
@@ -74,12 +78,13 @@ class OneHotEncoder:
         
         if self.vector_size > 0:
             all_tokens_count = [(k, v) for k,v in self.records.items()]
-            mostfrequent = nlargest(self.vector_size, all_tokens_count, key=lambda x:x[1])
+            mostfrequent = nlargest(self.vector_size - self.get_number_of_predefined_vectors(), all_tokens_count, key=lambda x:x[1])
             self.records = {entry[0]: entry[1] for entry in mostfrequent}
 
-        self.__vectors_dimension[1] = len(self.records) + 1
+        self.vectors_dimension[1] = len(self.records) + self.get_number_of_predefined_vectors()
 
     get_zero_vector = __get_zero_vector
+
 
 class SequentialOneHotEncoder(OneHotEncoder):
 
@@ -87,6 +92,45 @@ class SequentialOneHotEncoder(OneHotEncoder):
         result = []
         for sequence_records in record:
             result.append(super().transform(sequence_records))
+        
+        if len(result) == 0:
+            return ((self.get_zero_vector(),),)
+        return result
+
+
+class SequentialOneHotEncoderWithContext(OneHotEncoder):
+    
+    def __init__(self, context_length, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.context_length = context_length
+        
+    def get_number_of_predefined_vectors(self):
+        return super().get_number_of_predefined_vectors() + self.context_length
+    
+    def generate_sparse_vectors(self):
+        for i, (k, _) in enumerate(self.records.items(), start=self.context_length): # we force the context features to be at the first of the feature vector
+            self.vectors[k] = self.create_sparse_vector(i)
+        
+        del self.records
+    
+    def transform(self, record):
+        try:
+            result = [None]*len(record[0])
+        except:
+            result = []
+        get_context_properly_for_transformation = lambda x: x
+
+        if self.context_length == 1:
+            get_context_properly_for_transformation = lambda x: [x,]
+        dimensions_of_context = ((0,) * self.context_length, tuple(range(0, self.context_length)))
+        
+        for i, (context, sequence_records) in enumerate(zip(record[0], record[1])):
+            if len(sequence_records) == 0:
+                result[i] = (self.get_zero_vector(),)
+                continue
+            temp = super().transform(sequence_records) + \
+                [sparse_coo_tensor(dimensions_of_context, get_context_properly_for_transformation(context), size=self.vectors_dimension, dtype=float32)]
+            result[i] = temp
         
         if len(result) == 0:
             return ((self.get_zero_vector(),),)
