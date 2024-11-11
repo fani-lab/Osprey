f739b4528ceb5e40e50a77a031686b9c3b47f65f
Hossein Fani <hosseinfani@gmail.com>
2024-11-11 06:00:16 -0500

    Update message.py

--- src/cmn/message.py
+++ src/cmn/message.py
@@ -1,31 +1,26 @@
-class Message:
+class Message(Object):
     def __init__(
         self,
-        conversation_id: str,
-        message_line_number: str,
+        idx: str,
         author_id: str,
         time: str,
-        message_char_count: str,
-        message_word_count: str,
-        conversation_size: str,
-        number_of_authors: str,
-        message_text: str,
-        tagged_predator: str,
-        predatory_conversation: str,
+        n_chars: str,
+        n_words: str,
+        #conversation_size: str, should go to conversation class
+        #number_of_authors: str, should go to conversation class
+        text: str,
+        tagged_predator: str, #is this predatory message label or the author is predator?
+        #predatory_conversation: str, should go to conversation class
     ):
 
-        self.conversation_id = conversation_id
-        self.message_line_number = message_line_number
+        self.idx = message_line_number
         self.author_id = author_id
         self.time = time
-        self.message_char_count = message_char_count
-        self.message_word_count = message_word_count
-        self.conversation_size = conversation_size
-        self.number_of_authors = number_of_authors
-        self.message_text = message_text
+        self.n_chars = n_chars
+        self.n_words = n_words
+        self.text = text
         self.tagged_predator = tagged_predator
-        self.predatory_conversation = predatory_conversation
-
+        
     def __repr__(self):
         return (
             f"Message Line Number: {self.message_line_number},\n"
